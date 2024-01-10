29e0ef176c9612c3cd6152ed5d7de6700551999e
Ofodum Joseph <ofodumjoseph@yahoo.com>
2024-01-10 13:31:20 +0100

    altered the var to const to lock the variable value at compile time

--- first_application/lib/gradient_container.dart
+++ first_application/lib/gradient_container.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:first_application/style_text.dart';
 
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 
 class GradientContainer extends StatelessWidget{
 
   const GradientContainer({super.key});
+  
   @override
   Widget build(context){
       return Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
-              colors: const [
+              colors:  [
               Color.fromARGB(255, 121, 100, 157),
               Color.fromARGB(255, 7, 16, 98),
               ],
